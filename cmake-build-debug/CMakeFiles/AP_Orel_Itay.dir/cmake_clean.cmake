c6ae4fc29be985734c176ef28079e57f4d0f92ad
Itay <etelis2019@gmail.com>
2021-10-10 18:40:03 +0300

    Project Creation

---
+++ cmake-build-debug/CMakeFiles/AP_Orel_Itay.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AP_Orel_Itay.exe"
+  "AP_Orel_Itay.exe.manifest"
+  "AP_Orel_Itay.pdb"
+  "CMakeFiles/AP_Orel_Itay.dir/main.cpp.obj"
+  "libAP_Orel_Itay.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AP_Orel_Itay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
