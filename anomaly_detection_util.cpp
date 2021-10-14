b93ff46af3862174d8265db4e23402a1a1f5fa8e
Itay <etelis2019@gmail.com>
2021-10-14 16:01:59 +0300

    Update

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -1,4 +1,40 @@
 //
 // Created by Itay on 10/14/2021.
 //
+#include "cmath"
+#include "anomaly_detection_util.h"
 
+// returns the variance of X and Y
+float var(float* x, int size) {
+    float m = average(x, size);
+    float sum = 0;
+    for(int i = 1; i <= size; i++) {
+       sum += (float)pow(*(x + i) - m, 2);
+    }
+    return (float)(1.0/size) * sum;
+}
+
+float average(const float *pDouble, int size) {
+    float sum = 0;
+    for(int i = 1; i <= size; i++) {
+        sum += *(pDouble + i);
+    }
+    return float (1.0/size) * sum;
+}
+
+float cov(float* x, float* y, int size) {
+    float multiXY[size];
+    for (int i = 0; i < size; i++){
+        multiXY[i] = *(x +i) * *(y+i);
+    }
+    float eXY = average(multiXY, size);
+    float eX = average(x,size);
+    float eY = average(y,size);
+    return (eXY - (eX*eY));
+}
+
+// returns the Pearson correlation coefficient of X and Y
+float pearson(float* x, float* y, int size) {
+    float c = cov(x, y, size);
+    return c / (float)(sqrt(var(x, size)) * sqrt(var(y, size)));
+}
--- anomaly_detection_util.h
+++ anomaly_detection_util.h
@@ -5,4 +5,42 @@
 #ifndef AP_OREL_ITAY_ANOMALY_DETECTION_UTIL_H
 #define AP_OREL_ITAY_ANOMALY_DETECTION_UTIL_H
 
+// returns the variance of X and Y
+float var(float* x, int size);
+
+// returns the average of array pDouble.
+float average (const float *pDouble, int size);
+
+// returns the covariance of X and Y
+float cov(float* x, float* y, int size);
+
+// returns the Pearson correlation coefficient of X and Y
+float pearson(float* x, float* y, int size);
+
+class Line{
+public:
+    float a,b;
+    Line():a(0),b(0){}
+    Line(float a, float b):a(a),b(b){}
+    float f(float x){
+        return a*x+b;
+    }
+};
+
+class Point{
+public:
+    float x,y;
+    Point(float x, float y):x(x),y(x){}
+};
+
+// performs a linear regression and return s the line equation
+Line linear_reg(Point** points, int size);
+
+// returns the deviation between point p and the line equation of the points
+float dev(Point p,Point** points, int size);
+
+// returns the deviation between point p and the line
+float dev(Point p,Line l);
+
+
 #endif //AP_OREL_ITAY_ANOMALY_DETECTION_UTIL_H
