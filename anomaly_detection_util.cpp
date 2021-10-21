7d9bbf80badbfd3d78e9fbc4591748893b7f33b0
itay <etelis2019@gmail.com>
2021-10-21 12:50:55 +0300

    Equation change.

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -13,12 +13,15 @@ float var(float* x, int size) {
     // obtain the average of the values
     float xAverage = avg(x, size);
     float sum = 0;
+    float subBySize = 0;
     // iterate on the values and compute the sum
     for(int i = 0; i < size; i++) {
-       sum += (*x - xAverage)*(*x - xAverage);
-       x++;
+        sum += ((*x) * (*x));
+        x++;
     }
-    return ((float)1.0/(float)size) * sum;
+    subBySize = sum / (float)size;
+
+    return (subBySize) - (xAverage * xAverage);
 }
 
 float avg(float* x, int size) {
@@ -42,16 +45,21 @@ float multiplyAvg(float* x,float* y, int size) {
 }
 
 float cov(float* x, float* y, int size) {
-    float eXY = multiplyAvg(x,y,size);
     float eX = avg(x,size);
     float eY = avg(y,size);
-    return (eXY - (eX*eY));
+    float newX [size];
+    float newY [size];
+    for (int i = 0; i < size; i++){
+        newX[i] = *x - eX;
+        newY[i] = *y - eY;
+        x++,y++;
+    }
+    return (multiplyAvg(newX,newY,size));
 }
-
 
 float pearson(float* x, float* y, int size) {
     float c = cov(x, y, size);
-    return c / ((float)(sqrt(var(x, size)) * (float)sqrt(var(y, size))));
+    return c / (sqrtf(var(x, size)) * sqrtf(var(y, size)));
 }
 
 Line linear_reg(Point** points, int size) {
