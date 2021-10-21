5222d64ddfe37a7646547338c7793ee52ac29804
itay <etelis2019@gmail.com>
2021-10-21 12:02:07 +0300

    Added header descriptions for each function.

--- anomaly_detection_util.h
+++ anomaly_detection_util.h
@@ -3,26 +3,28 @@
 #ifndef ANOMALYDETECTORUTIL_H_
 #define ANOMALYDETECTORUTIL_H_
 
+// returns the average of a float array.
 float avg(const float* x, int size);
 
-// returns the variance of X and Y
+// returns the variance of X and Y.
 float var(float* x, int size);
 
-// returns the covariance of X and Y
+// returns the covariance of X and Y.
 float cov(float* x,  float* y, int size);
 
+// returns the average of both arrays x and y when multiplying each value.
 float multiplyAvg(const float* x, const float* y, int size);
 
-// returns the Pearson correlation coefficient of X and Y
+// returns the Pearson correlation coefficient of X and Y.
 float pearson(float* x, float* y, int size);
 
-// performs a linear regression and returns the line equation
+// performs a linear regression and returns the line equation.
 Line linear_reg(Point** points, int size);
 
-// returns the deviation between point p and the line equation of the points
+// returns the deviation between point p and the line equation of the points.
 float dev(Point p,Point** points, int size);
 
-// returns the deviation between point p and the line
+// returns the deviation between point p and the line.
 float dev(Point p,Line l);
 
 #endif
