a88cd501d63d54d0de5c6ed699d2acc64ac9ddb7
orel <orelbenshamai@gmail.com>
2021-10-28 13:39:42 +0300

    update

--- SimpleAnomalyDetector.cpp
+++ SimpleAnomalyDetector.cpp
@@ -1,15 +1,30 @@
 
 #include "SimpleAnomalyDetector.h"
 
+
+void freePoints(Point** points, size_t size){
+    for (int i = 0; i < size; i++){
+        free(points[i]);
+    }
+}
+
 void createPoints(Point** points, float* x, float* y, size_t size) {
     for(int i = 0; i < size; i++) {
         points[i] = new Point(x[i], y[i]);
     }
 }
 
-SimpleAnomalyDetector::SimpleAnomalyDetector() {
-	// TODO Auto-generated constructor stub
-
+void x(vector<AnomalyReport>& reports, Point** points,const correlatedFeatures&  cf, size_t size) {
+    Line reg = cf.lin_reg;
+    float threshold = cf.threshold;
+    string f1 = cf.feature1, f2 = cf.feature2;
+    for(int i = 0; i < size; i++) {
+        if(dev(*points[i], reg) > threshold) {
+            string description;
+            auto* r = new AnomalyReport(description.append(f1).append("-").append(f2), i);
+            reports.push_back(*r);
+        }
+    }
 }
 
 SimpleAnomalyDetector::~SimpleAnomalyDetector() {
@@ -21,7 +36,7 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
     auto data = ts.getData();
     map<string, vector<float>>::iterator it1;
     map<string, vector<float>>::iterator it2;
-    int size = it1->second.size();
+    size_t size = it1->second.size();
     it1 = data.begin();
     float max, p, *firstVector, *secondVector;
     string firstFeature, secondFeature;
@@ -45,6 +60,7 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
             createPoints(points, firstVector, secondVector, size);
             Line reg = linear_reg(points, it1->second.size());
             correlatedFeatures c = {firstFeature, secondFeature, p, reg, maxDev(points, reg, size) * 1.1};
+            freePoints(points, size);
             cf.push_back(c);
         }
         it1++;
@@ -53,6 +69,24 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts){
 }
 
 vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries& ts){
-	// TODO Auto-generated destructor stub
+    auto data = ts.getData();
+    string feature1, feature2;
+    float *v1, *v2;
+    size_t size;
+    Point** points;
+    vector<AnomalyReport> reports;
+    for(auto & i : cf) {
+        feature1 = i.feature1;
+        feature2 = i.feature2;
+        size = data.find(feature1)->second.size();
+        v1 = &data.find(feature1)->second[0];
+        v2 = &data.find(feature2)->second[0];
+        createPoints(points, v1, v2, size);
+        x(reports, points, i, size);
+        freePoints(points, size);
+    }
+    return reports;
 }
+
+
 
