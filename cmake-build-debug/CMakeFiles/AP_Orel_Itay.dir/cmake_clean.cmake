233dd6d9ba7016ee686762d7ada686803955725e
Itay <etelis2019@gmail.com>
2021-11-18 15:30:13 +0200

    Added basic points functions

---
+++ cmake-build-debug/CMakeFiles/AP_Orel_Itay.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "AP_Orel_Itay.exe"
+  "AP_Orel_Itay.exe.manifest"
+  "AP_Orel_Itay.pdb"
+  "CMakeFiles/AP_Orel_Itay.dir/MainTrain.cpp.obj"
+  "CMakeFiles/AP_Orel_Itay.dir/SimpleAnomalyDetector.cpp.obj"
+  "CMakeFiles/AP_Orel_Itay.dir/anomaly_detection_util.cpp.obj"
+  "CMakeFiles/AP_Orel_Itay.dir/minCircle.cpp.obj"
+  "CMakeFiles/AP_Orel_Itay.dir/timeseries.cpp.obj"
+  "libAP_Orel_Itay.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AP_Orel_Itay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
