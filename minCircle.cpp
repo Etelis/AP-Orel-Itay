958e8a1b1fdfee4eb1483f300c540d5c7ea5b3bf
orel <orelbenshamai@gmail.com>
2021-11-18 15:43:26 +0200

    update and

--- minCircle.cpp
+++ minCircle.cpp
@@ -2,11 +2,15 @@
 #include "minCircle.h"
 #include "algorithm"
 
-
 static std::default_random_engine randGen((std::random_device())());
 
-bool containsPoint(const Point* &point, const Circle &c) const {
+bool containsPoint(const Point &point, const Circle &c) {
     return c.radius <= c.center.distance(point);
+}
+
+Circle createCircleTwoPoints(Point &p1, Point &p2) {
+    Point m{(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
+    return Circle{m, (float)m.distance(p1)};
 }
 
 Circle findMinCircle(Point** points,size_t size) {
@@ -14,9 +18,20 @@ Circle findMinCircle(Point** points,size_t size) {
     // randomize order
     std::shuffle(v_points.begin(), v_points.end(), randGen);
     // create a circle defined by p1 and p2 as the starting circle
-    auto c = Circle(new Point(3,3), 5.5);
+    auto c = createCircleTwoPoints(*v_points.at(0), *v_points.at(1));
     for(int i = 2; i < size; i++) {
-        const Point* p = v_points.at(i);
+        const Point &p = *v_points.at(i);
+        if(!containsPoint(p, c)) {
 
+        }
     }
+    return c;
+}
+
+Circle createSmallestEnclosingCircleOnePoint(const vector<Point*> &v_points, size_t end, const Point &p) {
+
+}
+
+Circle createSmallestEnclosingCircleTwoPoint(const vector<Point*> &v_points, size_t end, const Point &p, const Point &q) {
+
 }
--- minCircle.h
+++ minCircle.h
@@ -18,12 +18,12 @@ public:
 	Point center;
 	float radius;
 	Circle(Point c,float r):center(c),radius(r){}
-
 };
 // --------------------------------------
 
 Circle findMinCircle(Point** points,size_t size);
-
-// you can add here additional methods
+Circle createCircleTwoPoints(Point &p1, Point &p2);
+Circle createSmallestEnclosingCircleOnePoint(const vector<Point*> &v_points, size_t end, const Point &p);
+Circle createSmallestEnclosingCircleTwoPoint(const vector<Point*> &v_points, size_t end, const Point &p, const Point &q);
 
 #endif /* MINCIRCLE_H_ */
