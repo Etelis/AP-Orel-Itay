268306184108119ddbadbde32b101eb2e09098f3
itay <etelis2019@gmail.com>
2021-12-14 20:37:44 +0200

    a

--- Server.cpp
+++ Server.cpp
@@ -2,14 +2,45 @@
 #include "Server.h"
 
 Server::Server(int port)throw (const char*) {
+    if ((serverFD = socket(AF_INET, SOCK_STREAM, 0) == 0)){
+        throw "socket failed";
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    if (bind(serverFD, (struct sockaddr *)&address, sizeof (address)) < 0){
+        throw "bind failed";
+
+    }
+    if (listen(serverFD, 3) < 0 ){
+        throw "listen failed";
+    }
 
 }
 
-void Server::start(ClientHandler& ch)throw(const char*){	
+void Server::start(ClientHandler& ch)throw(const char*){
+    int clientFD;
+    signal( SIGALRM, handle_alarm);
+    alarm(10);
+    socklen_t client_size = sizeof (address);
+
+    if ((clientFD = accept(serverFD, (struct sockaddr *)&address, &client_size) < 0)){
+        throw "error on accept";
+    }
+    alarm(0);
+
+    t = new thread([&ch, &clientFD](){
+        ch.handle(clientFD);
+        close(clientFD);
+    });
+
 }
 
 void Server::stop(){
 	t->join(); // do not delete this!
+    close(serverFD);
 }
 
 Server::~Server() {
